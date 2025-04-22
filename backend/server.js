fe8f7f77f54aa6b813c342e77e59076425081fa7
Panu32 <pranavvaidya2005@gmail.com>
2025-04-22 19:56:50 +0530

    creating Backend setup

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./src/db/index.js";
+import foodRouter from "./src/routes/foodRoute.js";
+
+// ✅ Load environment variables
+dotenv.config();
+
+// App config
+const app = express();
+const port = process.env.PORT || 4000;
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+// DB connection
+connectDB();
+// api endpoint
+app.use("/api/food",foodRouter)
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
+
+// Server start
+app.listen(port, () => {
+  console.log(`✅ Server started on http://localhost:${port}`);
+});
+
