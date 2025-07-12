e003643b263ec14d371fbbb743cf1e211b14f9d2
Panu32 <pranavvaidya2005@gmail.com>
2025-07-12 19:01:53 +0530

    checking admin order page error

--- backend/src/controllers/orderController.js
+++ backend/src/controllers/orderController.js
@@ -92,7 +92,7 @@ const userOrders = async (req, res) => {
 
 const listOrders = async (req, res) => {
     try {
-        const orders = await orderModel.find({}).
+        const orders = await orderModel.find({});
         res.json({ success: true, data: orders });
     } catch (error) {
         console.log(error);
